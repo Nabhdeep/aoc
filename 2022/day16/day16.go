f4617f210afb81c318a0472174d91d553800402d
nabhdeep <deep3700@gmail.com>
2024-08-31 00:32:15 +0530

    day16 part 2

--- 2022/day16/day16.go
+++ 2022/day16/day16.go
@@ -3,7 +3,6 @@ package day16
 import (
 	readinput "advent/readInput"
 	"fmt"
-	"sort"
 	"strconv"
 	"strings"
 )
@@ -11,27 +10,34 @@ import (
 type memoKey struct {
 	valve      string
 	time       int
-	openValves string
+	openValves uint64
+	player     int
 }
 
-var cache = make(map[memoKey]int)
+func (mk memoKey) hash() string {
+	return fmt.Sprintf("%s.%d.%d.%d", mk.valve, mk.time, mk.openValves, mk.player)
+}
+
+var cache = make(map[string]int)
+var valvePos = make(map[string]int)
 
 func Solve() {
 	valve := make(map[string]int)
 	paths := make(map[string][]string)
 	openVal := make(map[string]bool)
 	// cache := make(map[memoKey]int)
-	pathOfInputText := "./2022/day16/testinput.txt"
+	pathOfInputText := "./2022/day16/input.txt"
 	input := readinput.ReadFile(pathOfInputText)
 	splitInput := strings.Split(input, "\n")
-	parseInput(splitInput, &valve, &paths, &openVal)
+	parseInput(splitInput, &valve, &paths, &openVal, &valvePos)
 	// fmt.Println(valve)
 	// fmt.Println(paths)
 	solvePart1(&valve, &paths, openVal)
 	// fmt.Println(openVal)
 }
 
-func parseInput(input []string, valve *map[string]int, tunnles *map[string][]string, openVal *map[string]bool) {
+func parseInput(input []string, valve *map[string]int, tunnles *map[string][]string, openVal *map[string]bool, valvePos *map[string]int) {
+	var position int
 	for _, i := range input {
 		part := strings.Split(i, ";")
 		vl := strings.Split(part[0], "=")
@@ -39,6 +45,10 @@ func parseInput(input []string, valve *map[string]int, tunnles *map[string][]str
 		num, _ := strconv.Atoi(vl[1])
 		(*valve)[valveName[1]] = num
 
+		if _, exists := (*valvePos)[valveName[1]]; !exists {
+			(*valvePos)[valveName[1]] = position
+			position++
+		}
 		// tunnles
 		has_values := strings.Contains(part[1], "valves")
 		if has_values {
@@ -66,22 +76,29 @@ func solvePart1(valve *map[string]int, paths *map[string][]string, openValves ma
 	// and there are [players] which will play after me.
 	// fmt.Printf("Starting valves: %v\n", *valve)
 	// fmt.Printf("Starting paths: %v\n", *paths)
-	res := findMostPressure("AA", openValves, 30, 0, valve, paths)
-	// res2 := findMostPressure("AA", openValves, 26, 1, valve, paths)
-	fmt.Println("Res ", res)
-	// fmt.Println("Res ", res2)
+	initialMask := createBitmask(openValves)
+	// res := findMostPressure("AA", initialMask, 30, 0, valve, paths)
+	res2 := findMostPressure("AA", initialMask, 26, 1, valve, paths)
+	// fmt.Println("Res ", res)
+	fmt.Println("Res ", res2)
 }
-func mapToString(m map[string]bool) string {
-	var openValves []string
-	for valve, isOpen := range m {
+func createBitmask(openValves map[string]bool) uint64 {
+	var mask uint64
+	for valve, isOpen := range openValves {
 		if isOpen {
-			openValves = append(openValves, valve)
+			mask |= 1 << uint(valvePos[valve])
 		}
 	}
-	sort.Strings(openValves)
-	return strings.Join(openValves, ",")
+	return mask
 }
-func findMostPressure(v1 string, openValve map[string]bool, t int, player int, valve *map[string]int, paths *map[string][]string) int {
+func addToMask(mask uint64, valve string) uint64 {
+	return mask | (1 << uint(valvePos[valve]))
+}
+
+func isValveOpen(mask uint64, valve string) bool {
+	return (mask & (1 << uint(valvePos[valve]))) != 0
+}
+func findMostPressure(v1 string, openValve uint64, t int, player int, valve *map[string]int, paths *map[string][]string) int {
 	// fmt.Println("=====================================================================================================================")
 	// fmt.Println("All Valves: ", (*valve))
 	// fmt.Printf("Starting Valve: %s , time: %d \n", v1, t)
@@ -96,26 +113,28 @@ func findMostPressure(v1 string, openValve map[string]bool, t int, player int, v
 	key := memoKey{
 		valve:      v1,
 		time:       t,
-		openValves: mapToString(openValve),
+		openValves: openValve,
+		player:     player,
 	}
 	// fmt.Printf("Starting Valve: %s , time: %d \n", v1, t)
 	// fmt.Printf("Cache: %v\n", cache[key])
-	if cache[key] > 0 {
-		return cache[key]
+	hashKey := key.hash()
+	if cache[hashKey] > 0 {
+		return cache[hashKey]
 	}
 
 	maxPressure := 0
 	flowRate := (*valve)[v1]
 	// fmt.Printf("Valve %s flow rate: %d\n", v1, (*valve)[v1])
-	if flowRate > 0 && !openValve[v1] {
-		newOpenValve := make(map[string]bool)
-		for k, v := range openValve {
-			newOpenValve[k] = v
-		}
-		newOpenValve[v1] = true
+	if flowRate > 0 && !isValveOpen(openValve, v1) {
+		// newOpenValve := make(map[string]bool)
+		// for k, v := range openValve {
+		// 	newOpenValve[k] = v
+		// }
+		newMask := addToMask(openValve, v1)
 		pressureReleased := (t - 1) * flowRate
 		// fmt.Printf("Opening valve %s, releasing %d pressure\n", v1, pressureReleased)
-		maxPressure = pressureReleased + findMostPressure(v1, newOpenValve, t-1, player, valve, paths)
+		maxPressure = pressureReleased + findMostPressure(v1, newMask, t-1, player, valve, paths)
 	}
 
 	for _, p := range (*paths)[v1] {
@@ -123,6 +142,6 @@ func findMostPressure(v1 string, openValve map[string]bool, t int, player int, v
 		// fmt.Printf("Parsed valve: %s, flow rate: %d, tunnels: %v , maxPressure: %d\n", p, (*valve)[p], (*paths)[v1], maxPressure)
 	}
 
-	cache[key] = maxPressure
+	cache[hashKey] = maxPressure
 	return maxPressure
 }
